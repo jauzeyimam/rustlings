667b5d29475c2501ff052813e73a1bbace5a2e9d
Jauzey Imam <jauzey@poynt.co>
2019-05-29 18:44:03 -0700

    Completed all exercises

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -2,10 +2,11 @@
 // Address all the TODOs to make the tests pass!
 
 struct ColorClassicStruct {
-    // TODO: Something goes here
+    name: String,
+    hex: String,
 }
 
-struct ColorTupleStruct(/* TODO: Something goes here */);
+struct ColorTupleStruct(String, String);
 
 struct ColorUnitStruct;
 
@@ -16,7 +17,10 @@ mod tests {
     #[test]
     fn classic_c_structs() {
         // TODO: Instantiate a classic c struct!
-        // let green =
+        let green = ColorClassicStruct {
+            name: "green".to_string(),
+            hex: "#00FF00".to_string(),
+        };
 
         assert_eq!(green.name, "green");
         assert_eq!(green.hex, "#00FF00");
@@ -26,7 +30,7 @@ mod tests {
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct!
         // For more fun, use the field initialization shorthand.
-        // let green =
+        let green = ColorTupleStruct("green".to_string(), "#00FF00".to_string());
 
         assert_eq!(green.0, "green");
         assert_eq!(green.1, "#00FF00");
@@ -35,7 +39,7 @@ mod tests {
     #[test]
     fn unit_structs() {
         // TODO: Instantiate a unit struct!
-        // let green =
+        let green = ColorUnitStruct{};
 
         if let ColorUnitStruct = green {
             assert!(true);
--- exercises/test1.rs
+++ exercises/test1.rs
@@ -8,7 +8,13 @@
 // the price of an order of apples given the order amount. No hints this time!
 
 // Put your function here!
-// fn ..... {
+fn calculateprice(num: isize) -> isize {
+    if num > 40 {
+        num
+    } else {
+        num * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
